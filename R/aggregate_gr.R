37dc51c4caef5caf29f5ca8a02cca1a05acee472
Ning Yuan <ningyuan.sg@gmail.com>
2020-12-16 10:14:31 +0800

    Add intermediates argument to aggregate_gr

--- R/aggregate_gr.R
+++ R/aggregate_gr.R
@@ -4,6 +4,8 @@
 #'   \code{sr2gr}.
 #' @param tf2loci List of GRanges representing transcription factor to loci
 #'   bindings, obtained using \code{get_tf2loci}.
+#' @param intermediates If this function should return a list containing
+#'   intermediate data structures used in the computation.
 #' @param weight_fn Unary function to produce weights for Lancaster p-value
 #'   aggregation. This function will receive \code{mcols(gr)}, and should return
 #'   a numeric vector of the same length as \code{length(gr)}. See details.
@@ -21,7 +23,17 @@
 #'   maps to, if that column does not yet exists. So, you can use that column
 #'   in \code{weight_fn}.
 #'
-#' @return A tibble of results.
+#' @return If \code{intermediates=FALSE}, a tibble of results, one row per TF.
+#'   If \code{intermediates=TRUE}, a list with named elements:
+#'   \describe{
+#'     \item{gr}{The GRanges of the argument \code{gr}, possibly with an added
+#'     metadata columns "tfs_overlapped" --- see details.}
+#'     \item{grl}{A list of GRanges. Each GRanges is derived from overlapping
+#'       the argument \code{gr} with a transcription factor from the argument
+#'       \code{tf2loci}.}
+#'     \item{tfs}{The tibble of results which would have been returned by itself
+#'       if \code{intermediates=FALSE}}.
+#'   }
 #'
 #' @references Yi, L., Pimentel, H., Bray, N.L. et al. Gene-level
 #'   differential analysis at transcript-level resolution. Genome Biol
@@ -36,7 +48,7 @@
 #' @importFrom stats p.adjust median
 #' @importFrom tibble as_tibble
 aggregate_gr <- function(
-    gr, tf2loci,
+    gr, tf2loci, intermediates=FALSE,
     weight_fn=function(mcols)
       (1/mcols$tfs_overlapped) / sum(1/mcols$tfs_overlapped),
     filter_mapping_fn=function(n_rows)
@@ -45,6 +57,7 @@ aggregate_gr <- function(
   assert_that(not_empty(gr))
   assert_that(not_empty(tf2loci))
 
+  ## Preprocess to generate "tfs_overlapped" metadata column
   if (has_name(mcols(gr), "tfs_overlapped")) {
     message("Column tfs_overlapped already exists in gr, not overwriting.")
   } else {
@@ -62,21 +75,32 @@ aggregate_gr <- function(
     mcols(gr)$tfs_overlapped <- tfs_overlapped
   }
 
+  ## For each transcription factor, find its overlaps with gr, filtering by the
+  ## number of overlaps (keep only TSS in gr which overlap at least some number
+  ## of times with UniBind BED records).
   message(sprintf(
     "Calculating. Each dot represents a transcription factor (total: %d).",
     length(tf2loci)))
   # /FOR EACH/ transcription factor
-  results <- lapply(tf2loci, function(tfloci) {
+  grl <- lapply(tf2loci, function(tfloci) {
       message(".", appendLF=FALSE)
       gr_sub <- filter_by_num_overlaps(gr, tfloci, filter_mapping_fn)
-      pval <- lancaster(gr_sub$qval, weight_fn(mcols(gr_sub)))
-      tibble(pval=pval)
-    }) %>%
+      gr_sub
+    })
+
+  ## For each TSS-TF filtered overlap, aggregate their p-values.
+  tfs <- grl %>%
+    lapply(function(gr_sub) {
+        # NAs are ignored by lancaster
+        pval <- lancaster(gr_sub$qval, weight_fn(mcols(gr_sub)))
+        tibble(pval=pval)
+      }) %>%
     bind_rows() %>%
     mutate(TF=names(tf2loci)) %>%
     select(.data$TF, .data$pval) %>%  # reorder columns
-    mutate(qval=p.adjust(.data$pval, "BH"))
+    mutate(qval=p.adjust(.data$pval, "BH")) %>%
+    arrange(.data$pval)
   message()  # just for the newline
 
-  results %>% arrange(.data$pval)
+  if (intermediates) list(gr=gr, grl=grl,tfs=tfs) else tfs
 }
